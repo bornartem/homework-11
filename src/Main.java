e68fd92f8d1f82e69cc0e551178cc6d8a6dd8b90
bornartem <born.artem@gmail.com>
2023-08-13 12:28:13 +0300

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,51 @@
 public class Main {
-    public static void printYear(int newYear) { // Метод 1 для Задачи1
-        System.out.println(newYear + " год -  високосный год");
-    }
-    public static void printYear1(int newYear) { // Метод 2 для Задачи1
-        System.out.println(newYear + " год - невисокосный год");
+    public static void printYear(int newYear) { // Метод для Задачи1
+        if (newYear % 4 == 0 && newYear % 100 != 0) {
+            System.out.println(newYear + " год -  високосный год");
+        } else if (newYear % 400 == 0) {
+            System.out.println(newYear + " год -  високосный год");
+        } else {
+            System.out.println(newYear + " год - невисокосный год");
+        }
     }
 
     public static void main(String[] args) { // Задача 1
-        int year = 2009;
-        if (year % 4 == 0 && year % 100 != 0) {
-            printYear(year);
-        } else if (year % 400 == 0) {
-            printYear(year);
-        } else {
-            printYear1(year);
-        }
+        int year = 2400;
+        printYear(year);
 
-        int clientOS1 = 0;// Задача 2
+
+        int clientOS = 1;// Задача 2
         int clientDeviceYear = 2023;
-        if (clientOS1 == 0 && clientDeviceYear < 2015) {
-            oldIos(clientOS1, clientDeviceYear);
-        } else if (clientOS1 == 1 && clientDeviceYear < 2015) {
-            oldAndroid(clientOS1, clientDeviceYear);
-        } else if (clientOS1 == 0 && clientDeviceYear >= 2015) {
-            newIos(clientOS1, clientDeviceYear);
+        versionIos (clientOS, clientDeviceYear);
+
+        int deliveryDistance = 110;// Задача 3
+        String deliveryDays = deliveryDays(deliveryDistance);
+        System.out.println(deliveryDays);
+    } //end method main
+
+    public static String deliveryDays(int newDeliveryDistance) { // Метод для задачи 3
+        if (newDeliveryDistance <= 20) {
+            return "Доставка в пределах суток ";
+        } else if (newDeliveryDistance > 20 && newDeliveryDistance <= 60) {
+            return "Доставка двое суток";
+        } else if (newDeliveryDistance > 60 && newDeliveryDistance <= 100) {
+            return "Доставка трое суток";
         } else {
-            newAndoid();
+            return ("Доставки нет");
         }
+    }
 
-            int deliveryDistance = 110;// Задача 3
-            String deliveryDays = deliveryDays (deliveryDistance);
-            System.out.println(deliveryDays);
-        } //end method main
-        public static String deliveryDays(int newDeliveryDistance) { // Метод для задачи 3
-            if (newDeliveryDistance <= 20) {
-                return "Доставка в пределах суток ";
-            } else if (newDeliveryDistance > 20 && newDeliveryDistance <= 60) {
-                return "Доставка двое суток";
-            } else if (newDeliveryDistance > 60 && newDeliveryDistance <= 100) {
-                return "Доставка трое суток";
-            } else {
-                return ("Доставки нет");
-            }
+    public static void versionIos(int newclientOS, int newclientDeviceYear) { // Метод для Задачи2
+        if (newclientOS == 0 && newclientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        } else if (newclientOS == 1 && newclientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (newclientOS == 0 && newclientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
-
-    public static void oldIos(int newclientOS, int newclientDeviceYear) { // Метод 1 для Задачи2
-        System.out.println("Установите облегченную версию приложения для IOS по ссылке");
     }
-    public static void oldAndroid(int newclienOS, int newclientDeviceYear) { // Метод 2 для Задачи2
-        System.out.println("Установите облегченную версию приложения для Android по ссылке");
-    }
-    public static void newIos(int newclienOS, int newclientDeviceYear) { // Метод 3 для Задачи2
-        System.out.println("Установите версию приложения для IOS по ссылке");
-    }
-    public static void newAndoid() { // Метод 4 для Задачи2
-        System.out.println("Установите версию приложения для Android по ссылке");
-    }
-
-
-
 }
 
 
